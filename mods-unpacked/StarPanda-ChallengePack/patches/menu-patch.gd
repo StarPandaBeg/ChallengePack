666c991b72b5bc6cd6d64d7d328bbda1d5162ed3
StarPandaBeg <kshmurakov@mail.ru>
2024-05-17 12:05:50 +0300

    Refactor: Rewrite MenuPatch to use original start button
    Now patch won't spawn custom start button and will rebind original one

--- mods-unpacked/StarPanda-ChallengePack/patches/menu-patch.gd
+++ mods-unpacked/StarPanda-ChallengePack/patches/menu-patch.gd
@@ -1,59 +1,33 @@
 extends "./patch.gd"
 
-const STPND_CHALLENGEPACK_LOG := "StarPanda-ChallengePack:MenuPatch2"
-
-const ButtonClass = preload("res://scripts/ButtonClass.gd")
-const CPButtonUtil = preload("../util/CPButtonUtil.gd")
 const CPGameOptionsMenuManager = preload("../util/CPGameOptionsMenuManager.gd")
+const STPND_CHALLENGEPACK_LOG := "StarPanda-ChallengePack:MenuPatch"
 
 var manager: CPGameOptionsMenuManager
-var menu_custom_start_button: ButtonClass
-var menu_real_start_button: ButtonClass
-
-var button_fake_start_config := {
-	"ui_root": "Camera/dialogue UI/menu ui",
-	"label_text": "start",
-	"label_name": "button_challengepack_start custom",
-	"button_name": "true button_challengepack_start custom",
-	"logic_name": "button class_challengepack_start custom",
-	"label_position": Vector2(0, 336),
-	"button_position": Vector2(447, 339),
-	"button_scale": Vector2(8.182, 3.012),
-}
 
 func _init():
 	scene_name = "menu"
-	
-func _apply(root: Node) -> bool:
-	_create_custom_start_btn(root)
-	_inject_custom_menu(root)
-	_instantiate_custom_manager(root)
-	
-	ModLoaderLog.info("Applied menu patch!", STPND_CHALLENGEPACK_LOG)
-	return true
-	
-func _create_custom_start_btn(root: Node):
-	menu_custom_start_button = CPButtonUtil.createButtonWithConfig(root, button_fake_start_config)
-	menu_custom_start_button.connect("is_pressed", func(): _on_custom_start_click(root))
-	ModLoaderLog.debug("Fake 'Start' button created", STPND_CHALLENGEPACK_LOG)
-		
-func _inject_custom_menu(root: Node) -> void:
-	var menu_manager = root.get_node("standalone managers/menu manager")
-	
-	menu_real_start_button = menu_manager.buttons[0]
-	CPButtonUtil.setState(menu_real_start_button, false)
-	CPButtonUtil.setState(menu_custom_start_button, true)
-	
-	menu_manager.buttons[0] = menu_custom_start_button
-	# menu_manager.screen_main[4] = menu_custom_start_button.ui
 
-func _on_custom_start_click(root: Node) -> void:
-	manager.show()
-	CPButtonUtil.setState(menu_custom_start_button, false)
+func _apply(root: Node):
+	ModLoaderLog.debug("Apply phase started", STPND_CHALLENGEPACK_LOG)
+	self.manager = _create_custom_manager(root)
+	ModLoaderLog.debug("Custom menu created", STPND_CHALLENGEPACK_LOG)
 	
-func _instantiate_custom_manager(root: Node) -> void:
+	var menu_manager = root.get_node("standalone managers/menu manager")
+	var mod_manager = self.manager
+	
+	menu_manager.buttons[0].disconnect("is_pressed", menu_manager.Start);
+	menu_manager.buttons[0].connect("is_pressed", _on_custom_start_click)
+	ModLoaderLog.debug("Start button rebind successfull", STPND_CHALLENGEPACK_LOG)
+	ModLoaderLog.debug("Apply phase finished", STPND_CHALLENGEPACK_LOG)
+	return true
+
+func _on_custom_start_click():
+	ModLoaderLog.debug("Trying to show mod menu", STPND_CHALLENGEPACK_LOG)
+	self.manager.show()
+
+func _create_custom_manager(root: Node):
 	var parent = root.get_node("standalone managers")
 	var manager = CPGameOptionsMenuManager.new(root)
 	parent.add_child(manager)
-	self.manager = manager
-	
+	return manager
