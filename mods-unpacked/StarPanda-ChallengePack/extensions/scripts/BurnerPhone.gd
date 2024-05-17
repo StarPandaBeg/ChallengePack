6c9a421c18395789c4025a7bb1d111ef5db86dc6
StarPandaBeg <kshmurakov@mail.ru>
2024-05-17 15:05:03 +0300

    Chore: Add explanatory comment

--- mods-unpacked/StarPanda-ChallengePack/extensions/scripts/BurnerPhone.gd
+++ mods-unpacked/StarPanda-ChallengePack/extensions/scripts/BurnerPhone.gd
@@ -1,1 +1,4 @@
 extends "res://scripts/BurnerPhone.gd"
+
+# No additional code
+# Extension added due to strange import errors when ShellSpawner is extended
--- mods-unpacked/StarPanda-ChallengePack/extensions/scripts/ShellExamine.gd
+++ mods-unpacked/StarPanda-ChallengePack/extensions/scripts/ShellExamine.gd
@@ -1,1 +1,4 @@
 extends "res://scripts/ShellExamine.gd"
+
+# No additional code
+# Extension added due to strange import errors when ShellSpawner is extended
